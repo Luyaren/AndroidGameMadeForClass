9e9f4a859b8b88139481dc2130a1c879e8697e83
Kurt Jarvi <20047m@gmail.com>
2018-02-07 02:17:24 +0000

    Merged PR 5: Merge kurt_dev to master
    Added initial Android app boilerplate code from Android Studio.

---
+++ app/src/main/java/io/github/fake/robotbois/robopuzzle/MainActivity.kt
@@ -0,0 +1,23 @@
+package io.github.fake.robotbois.robopuzzle
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+/*
+
+class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+ */
